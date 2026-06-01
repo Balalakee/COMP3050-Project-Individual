38abd373467032f6c9dc479ca1bb0f4389112610
Blake Kouzeleas <bkouze2014@gmail.com>
2026-06-01 23:28:17 +1000

    Added ansible

--- infra/main.tf
+++ infra/main.tf
@@ -11,38 +11,109 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-variable "key_pair_name" {}
-variable "instance_type" {}
-variable "ssh_location" {}
+variable "key_pair_name" {
+  description = "Name of an existing EC2 key pair for SSH access"
+  type        = string
+}
 
-resource "aws_security_group" "COMP3050-Project" {
-  name        = "COMP3050-Project-sg"
-  description = "Allow SSH and GAMEPLAY"
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t3.micro"
+}
 
-  lifecycle {
-    create_before_destroy = true
-  }
+data "aws_ssm_parameter" "ami" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-default-x86_64"
+}
+
+resource "aws_security_group" "questshaper" {
+  name        = "questshaper-sg"
+  description = "Allow SSH, QuestShaper game server, and monitoring ports"
 
   ingress {
     description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.ssh_location]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
-    description = "Game Server"
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "QuestShaper Game Server"
     from_port   = 8000
     to_port     = 8000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Grafana"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Prometheus"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+   ingress {
+     description = "Loki"
+     from_port   = 3100
+     to_port     = 3100
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
   egress {
+    description = "Allow outbound internet access"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_instance" "questshaper" {
+  ami                    = data.aws_ssm_parameter.ami.value
+  instance_type          = var.instance_type
+  key_name               = var.key_pair_name
+  vpc_security_group_ids = [aws_security_group.questshaper.id]
+
+  user_data = <<-EOF
+    #!/bin/bash
+    yum update -y
+    dnf install -y docker git
+    systemctl start docker
+    systemctl enable docker
+    usermod -aG docker ec2-user
+  EOF
+
+  tags = {
+    Name    = "QuestShaper"
+    Project = "COMP3050"
+  }
+}
+
+output "questshaper_public_ip" {
+  description = "Public IP address of the QuestShaper EC2 instance"
+  value       = aws_instance.questshaper.public_ip
+}
+
+output "questshaper_instance_id" {
+  description = "QuestShaper EC2 instance ID"
+  value       = aws_instance.questshaper.id
 }
